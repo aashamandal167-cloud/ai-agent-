4474ce445db2bfdd053b29b2d75be64e9d91b6ab
aashamandal167-cloud <aashamandal167@gmail.com>
2026-05-15 14:19:28 +0530

    Implement sidebar menu and chat functionality

--- script.js
+++ script.js
@@ -5,3 +5,88 @@ function openSidebar() {
 function closeSidebar() {
   document.getElementById("sidebar").classList.remove("active");
 }
+
+/* MENU CLICK */
+
+const menuItems = document.querySelectorAll(".menu li");
+
+menuItems.forEach(item => {
+  item.addEventListener("click", () => {
+
+    // remove old active
+    menuItems.forEach(i => i.classList.remove("active-menu"));
+
+    // add new active
+    item.classList.add("active-menu");
+
+    // chat message change
+    const chatArea = document.querySelector(".chat-area");
+
+    chatArea.innerHTML = `
+      <div class="bot-message">
+        ${item.innerText} section opened successfully 🚀
+      </div>
+    `;
+
+    closeSidebar();
+  });
+});
+
+/* SEND MESSAGE */
+
+const sendBtn = document.querySelector(".send-btn");
+const textarea = document.querySelector("textarea");
+const chatArea = document.querySelector(".chat-area");
+
+sendBtn.addEventListener("click", sendMessage);
+
+textarea.addEventListener("keydown", function(e) {
+  if (e.key === "Enter" && !e.shiftKey) {
+    e.preventDefault();
+    sendMessage();
+  }
+});
+
+function sendMessage() {
+
+  const message = textarea.value.trim();
+
+  if(message === "") return;
+
+  // user message
+  const userDiv = document.createElement("div");
+  userDiv.className = "user-message";
+  userDiv.innerText = message;
+
+  chatArea.appendChild(userDiv);
+
+  // fake ai reply
+  const botDiv = document.createElement("div");
+  botDiv.className = "bot-message";
+  botDiv.innerText = "Raaz Chandrvashi is processing your request... 🚀";
+
+  chatArea.appendChild(botDiv);
+
+  textarea.value = "";
+
+  // auto scroll
+  chatArea.scrollTop = chatArea.scrollHeight;
+}
+
+/* PLUS BUTTON */
+
+const plusBtn = document.querySelector(".plus-btn");
+
+plusBtn.addEventListener("click", () => {
+
+  const botDiv = document.createElement("div");
+
+  botDiv.className = "bot-message";
+
+  botDiv.innerText =
+    "New AI task creator coming soon 🚀";
+
+  chatArea.appendChild(botDiv);
+
+  chatArea.scrollTop = chatArea.scrollHeight;
+});
