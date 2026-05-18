74fc74e6de90bc389e6f99f76914c6f770ec6fa1
aashamandal167-cloud <aashamandal167@gmail.com>
2026-05-18 14:41:56 +0530

    Add sidebar functions and send button alert

---
+++ app.js
@@ -0,0 +1,15 @@
+alert("APP JS WORKING");
+
+function openSidebar() {
+  alert("MENU WORKING");
+}
+
+function closeSidebar() {
+  alert("CLOSE WORKING");
+}
+
+const sendBtn = document.querySelector(".send-btn");
+
+sendBtn.addEventListener("click", () => {
+  alert("SEND WORKING");
+});
