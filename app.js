af101433e0729ec682982289abd2e2baba89129a
aashamandal167-cloud <aashamandal167@gmail.com>
2026-05-18 15:28:46 +0530

    Remove sidebar functions and alert messages
    Removed alert messages and sidebar functions.

--- app.js
+++ app.js
@@ -1,15 +1,1 @@
-alert("APP JS WORKING");
 
-function openSidebar() {
-  alert("MENU WORKING");
-}
-
-function closeSidebar() {
-  alert("CLOSE WORKING");
-}
-
-const sendBtn = document.querySelector(".send-btn");
-
-sendBtn.addEventListener("click", () => {
-  alert("SEND WORKING");
-});
