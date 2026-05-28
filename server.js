e94f7049e7a9ae33afc54f7eee6b14b7e386fd35
aashamandal167-cloud <aashamandal167@gmail.com>
2026-05-28 10:37:56 +0530

    Remove duplicate app.listen and PORT declaration
    Removed duplicate server listening code.

--- server.js
+++ server.js
@@ -555,15 +555,6 @@ error:err.message
 
 });
 
-
-
-const PORT = process.env.PORT || 10000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
-
 const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
