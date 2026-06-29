fa44aeb69235705c60dfa3b8e18fd060892ac406
aashamandal167-cloud <aashamandal167@gmail.com>
2026-06-29 10:47:14 +0530

    Correct export syntax and add new brain exports
    Fixed export syntax and added new brain modules.

--- brains/index.js
+++ brains/index.js
@@ -1,4 +1,10 @@
-export { default as discoveryBrain } from "./discoveryBrain.js";
+exportexport { default as discoveryBrain } from "./discoveryBrain.js";
 export { default as emotionBrain } from "./emotionBrain.js";
 export { default as storyBrain } from "./storyBrain.js";
 export { default as demoBrain } from "./demoBrain.js";
+
+export { default as negotiationBrain } from "./negotiationBrain.js";
+export { default as dealBrain } from "./dealBrain.js";
+export { default as paymentBrain } from "./paymentBrain.js";
+export { default as objectionBrain } from "./objectionBrain.js";
+export { default as followupBrain } from "./followupBrain.js";
