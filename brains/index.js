19e5cd90bf3fd3e11a4042b5e1fc131cf1059b43
aashamandal167-cloud <aashamandal167@gmail.com>
2026-06-29 14:08:41 +0530

    Fix export syntax in index.js

--- brains/index.js
+++ brains/index.js
@@ -1,8 +1,7 @@
-exportexport { default as discoveryBrain } from "./discoveryBrain.js";
+export { default as discoveryBrain } from "./discoveryBrain.js";
 export { default as emotionBrain } from "./emotionBrain.js";
 export { default as storyBrain } from "./storyBrain.js";
 export { default as demoBrain } from "./demoBrain.js";
-
 export { default as negotiationBrain } from "./negotiationBrain.js";
 export { default as dealBrain } from "./dealBrain.js";
 export { default as paymentBrain } from "./paymentBrain.js";
