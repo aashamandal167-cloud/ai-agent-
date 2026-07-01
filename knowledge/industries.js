b603ff86ac0067ca4af52f820e0e1f1ae1b1d1ab
aashamandal167-cloud <aashamandal167@gmail.com>
2026-07-01 15:21:28 +0530

    Add industries data structure with categories

---
+++ knowledge/industries.js
@@ -0,0 +1,94 @@
+const industries = {
+
+  Retail: [
+    "Fashion Store",
+    "Boutique",
+    "Garment Shop",
+    "Kirana Store",
+    "Supermarket",
+    "Electronics Shop",
+    "Mobile Shop",
+    "Furniture Store",
+    "Jewellery Shop",
+    "Medical Store",
+    "Book Store",
+    "Gift Shop",
+    "Toy Shop",
+    "Footwear Shop",
+    "Cosmetics Shop"
+  ],
+
+  Food: [
+    "Restaurant",
+    "Cafe",
+    "Hotel",
+    "Bakery",
+    "Sweet Shop",
+    "Fast Food",
+    "Cloud Kitchen",
+    "Tea Stall",
+    "Juice Shop"
+  ],
+
+  Health: [
+    "Clinic",
+    "Hospital",
+    "Gym",
+    "Fitness Center",
+    "Yoga Center",
+    "Spa",
+    "Salon",
+    "Dental Clinic",
+    "Physiotherapy Clinic"
+  ],
+
+  Education: [
+    "School",
+    "College",
+    "Coaching Center",
+    "Training Institute",
+    "Computer Institute",
+    "Music Academy"
+  ],
+
+  Professional: [
+    "CA",
+    "Lawyer",
+    "Consultant",
+    "Architect",
+    "Freelancer",
+    "Marketing Agency",
+    "Software Company"
+  ],
+
+  RealEstate: [
+    "Builder",
+    "Real Estate Agency",
+    "Property Dealer",
+    "Construction Company"
+  ],
+
+  Automobile: [
+    "Car Showroom",
+    "Bike Showroom",
+    "Garage",
+    "Car Service Center",
+    "Tyre Shop"
+  ],
+
+  Travel: [
+    "Travel Agency",
+    "Tour Operator",
+    "Taxi Service"
+  ],
+
+  Manufacturing: [
+    "Factory",
+    "Manufacturer",
+    "Wholesaler",
+    "Distributor"
+  ]
+
+};
+
+export default industries;
