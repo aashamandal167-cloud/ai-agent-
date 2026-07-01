fb8c5ba19ea93d982c5f2e86cbc3b1058aedd508
aashamandal167-cloud <aashamandal167@gmail.com>
2026-07-01 14:52:15 +0530

    Add businessProblems module with common issues

---
+++ knowledge/businessProblems.js
@@ -0,0 +1,53 @@
+const businessProblems = {
+
+  Gym: [
+    "Low Membership",
+    "Less Walk-in Customers",
+    "Weak Google Presence",
+    "Low Online Booking",
+    "Poor Branding"
+  ],
+
+  Restaurant: [
+    "Less Table Booking",
+    "Low Online Orders",
+    "Weak Google Reviews",
+    "Poor Branding",
+    "Less Repeat Customers"
+  ],
+
+  Hotel: [
+    "Low Room Booking",
+    "Less Direct Booking",
+    "Dependence on OTA",
+    "Weak Online Presence",
+    "Low Customer Trust"
+  ],
+
+  Salon: [
+    "Less Appointment Booking",
+    "Low Repeat Customers",
+    "Weak Branding",
+    "Poor Google Visibility",
+    "Less New Customers"
+  ],
+
+  Clinic: [
+    "Less Patient Enquiries",
+    "Weak Online Presence",
+    "Low Appointment Booking",
+    "Poor Google Visibility",
+    "Low Trust"
+  ],
+
+  Fashion Store: [
+    "Low Sales",
+    "Less Footfall",
+    "Weak Online Presence",
+    "Poor Branding",
+    "Customers Buying Online"
+  ]
+
+};
+
+export default businessProblems;
