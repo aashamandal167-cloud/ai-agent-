80892a80cfbf3a87a52e40705724e923a2ed509a
aashamandal167-cloud <aashamandal167@gmail.com>
2026-07-15 17:32:49 +0530

    Update import paths for brain modules

--- services/brainManager.js
+++ services/brainManager.js
@@ -6,13 +6,13 @@
  * ==========================================================
  */
 
-import discoveryBrain from "./discoveryBrain.js";
-import storyBrain from "./storyBrain.js";
-import demoBrain from "./demoBrain.js";
-import dealBrain from "./dealBrain.js";
-import negotiationBrain from "./negotiationBrain.js";
-import paymentBrain from "./paymentBrain.js";
-import followupBrain from "./followupBrain.js";
+import discoveryBrain from "../brains/discoveryBrain.js";
+import storyBrain from "../brains/storyBrain.js";
+import demoBrain from "../brains/demoBrain.js";
+import dealBrain from "../brains/dealBrain.js";
+import negotiationBrain from "../brains/negotiationBrain.js";
+import paymentBrain from "../brains/paymentBrain.js";
+import followupBrain from "../brains/followupBrain.js";
 
 const BRAINS = {
 
